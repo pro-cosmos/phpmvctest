92ff48ecda3fff344604bec29416b64bd9e5f69c
SboevKonstantin <sboevkm@gmail.com>
2014-10-29 14:02:10 +0400

    4. Доработайте проект так, чтобы исключить подобные (п.3) ошибки при добавлении новых вариантов настроек; 5. Доработайте проект так, чтобы запрос на несуществующую ссылку не приводил к PHP Fatal error;

--- lib/test/Loader.php
+++ lib/test/Loader.php
@@ -11,9 +11,12 @@ class Loader
         return spl_autoload_register(array(__CLASS__, 'includeClass'));
     }
 
-    public static function includeClass($class)
+   public static function includeClass($class)
     {
-        require_once(PROJECTROOT . '/' . self::$_libdir . '/' . strtr($class, '_\\', '//') . '.php');
+      //5. Доработайте проект так, чтобы запрос на несуществующую ссылку не приводил к PHP Fatal error;
+       $classFile = PROJECTROOT . '/' . self::$_libdir . '/' . strtr($class, '_\\', '//') . '.php';
+       if (file_exists($classFile))
+          require_once($classFile);
     }
 }
 
